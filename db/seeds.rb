410bf15f031924287f87579449e103115abb6309
m3thom <nemonakub@gmail.cpm>
2019-06-25 17:13:04 +0700

    Create seed file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+      title: "My blog post #{blog}.",
+      body: "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+    )
+end
+
+puts "10blog added"
+
+5.times do |skill|
+  Skill.create!(
+      title: "Rails #{skill}",
+      percent_utilized: 15+skill,
+    )
+end
+
+puts "5skills added"
+
+9.times do |port_i|
+  Portfolio.create!(
+      title: "Portfolio title #{port_i}",
+      main_image: "https://via.placeholder.com/600/09f/fff.png",
+      thumb_image: "https://via.placeholder.com/300/09f/fff.png",
+    )
+end
+
+
+
+
