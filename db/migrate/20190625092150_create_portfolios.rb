972cb41da19b17a2a7d832953f3415882a14f651
m3thom <nemonakub@gmail.cpm>
2019-06-25 16:28:52 +0700

    added portfo via res gener

---
+++ db/migrate/20190625092150_create_portfolios.rb
@@ -0,0 +1,11 @@
+class CreatePortfolios < ActiveRecord::Migration[5.2]
+  def change
+    create_table :portfolios do |t|
+      t.string :title
+      t.text :main_image
+      t.text :thumb_image
+
+      t.timestamps
+    end
+  end
+end
