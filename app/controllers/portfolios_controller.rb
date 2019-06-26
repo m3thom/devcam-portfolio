bad6ea53e43e078fc0cac0b50bbb9072465bb3e0
m3thom <nemonakub@gmail.cpm>
2019-06-26 12:37:22 +0700

    add destroy action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -33,6 +33,19 @@ class PortfoliosController < ApplicationController
 
   def show
     @portfolio_item = Portfolio.find(params[:id])
-    
+  end
+
+  def destroy
+    #look up
+    @portfolio_item = Portfolio.find(params[:id])
+
+    #destroy/delete the record
+    @portfolio_item.destroy
+
+    #redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Port was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 end
