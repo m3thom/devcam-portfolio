475ee32e0186f78f9154e237f2541cb84e4ee4c9
m3thom <nemonakub@gmail.cpm>
2019-06-26 13:04:48 +0700

    custom resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  resources :portfolios
+
+  resources :portfolios, except: [:show]
+
+  get "portfolio/:id", to: "portfolios#show", as: "portfolio_show"
+
   #ข้างหน้าคือชื่อ path(เปลี่ยนเป็นอะไรก็ได้) ข้างหลังคือบอกให้ลิ้งค์ไป
   get "about-me",to:'pages#about'
   get "contact",to:'pages#contact'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   #url เปล่าให้เป็นหน้า home
   root to:'pages#home'
 
