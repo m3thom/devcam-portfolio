40dc32149538cc7c1906900479573398d9022cba
m3thom <nemonakub@gmail.cpm>
2019-06-26 12:51:49 +0700

    custome routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  #ข้างหน้าคือชื่อ path(เปลี่ยนเป็นอะไรก็ได้) ข้างหลังคือบอกให้ลิ้งค์ไป
+  get "about-me",to:'pages#about'
+  get "contact",to:'pages#contact'
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #url เปล่าให้เป็นหน้า home
+  root to:'pages#home'
+
 end
