e21ea72a4c3e58413c4412b966ca9dd658089326
m3thom <nemonakub@gmail.cpm>
2019-07-01 01:09:53 +0700

    refactor outside url with SetSource moduel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,7 @@ class ApplicationController < ActionController::Base
     #protect_from_forgery with: :exception
     include DeviseWhitelist
 
-    before_action :set_source
+    include SetSource
 
-    def set_source
-        session[:source] = params[:q] if params[:q]
-    end
+    
 end
