038262eb7d645eab353c069f95bcb1b861845331
m3thom <nemonakub@gmail.cpm>
2019-07-02 16:42:04 +0700

    created a comemnt broadcast, config routes, update comment model callback action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :comments
   devise_for :users, path: '', path_names: {sign_in: 'login', sig_out: 'logout', sign_up: 'register'}
   resources :portfolios, except: [:show]
 
@@ -17,6 +16,8 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  mount ActionCable.server => '/cable'
   
   #url เปล่าให้เป็นหน้า home
   root to:'pages#home'
