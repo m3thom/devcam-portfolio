35b55161ec884127374038ed1a413a54bbf467fb
m3thom <nemonakub@gmail.cpm>
2019-07-02 18:36:09 +0700

    not complete realtime comment LOL

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
     def create
-        @comment = current_user.comments.build(strong_params)
+        @comment = current_user.comments.build(comment_params)
     end
 
     private
-    def strong_params
+    def comment_params
         params.require(:comment).permit(:content)
     end
 end
