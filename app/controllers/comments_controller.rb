456f1053087510e3ed0e56ed6aa21040196a7233
m3thom <nemonakub@gmail.cpm>
2019-07-02 15:12:01 +0700

    imped comment for blog

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+    def create
+        @comment = current_user.comments.build(strong_params)
+    end
+
+    private
+    def strong_params
+        params.require(:comment).permit(:content)
+    end
 end
