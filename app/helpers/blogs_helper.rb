4ada1c82c4b68bed39c88ba2159c258a82676449
m3thom <nemonakub@gmail.cpm>
2019-07-03 12:29:48 +0700

    imped markdown language

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,19 @@
 module BlogsHelper
+
+    class CodeRayify < Redcarpet::Render::HTML
+        def block_code(code, language)
+            CodeRay.scan(code, language).div
+        end
+    end
+    def markdown(text)
+        coderayified = CodeRayify.new(filter_html: true,hard_wrap: true)
+        options = {
+            fenced_code_blocks: true,
+            no_intra_emphasis: true,
+            autolink: true,
+            lax_html_block: true
+        }
+        makrdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+        makrdown_to_html.render(text).html_safe
+    end
 end
