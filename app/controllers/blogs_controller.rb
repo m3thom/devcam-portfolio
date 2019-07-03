acc323693e803f5b3cc842a00135cb4de14385f4
m3thom <nemonakub@gmail.cpm>
2019-07-03 10:46:40 +0700

    imped hide draft post of blog form none :site_admin user

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -16,11 +16,14 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-    @blog = Blog.includes(:comments).friendly.find(params[:id])
-    @comment = Comment.new
-    
-    @page_title = @blog.title
-    @seo_keywords = @blog.body
+    if logged_in?(:site_admin) || @blog.published?
+      @blog = Blog.includes(:comments).friendly.find(params[:id])
+      @comment = Comment.new
+      
+      @page_title = @blog.title
+      @seo_keywords = @blog.body
+    else redirect_to blogs_path, notice: "You have no authorize to acces to this page"
+    end
   end
 
   # GET /blogs/new
